11f5faa45b044bceea03c958a35507053f5e3f3d
Lara Kayaalp <me@lara.lv>
2024-03-11 12:25:55 +0300

    add twilight crates

--- README.md
+++ README.md
@@ -4,11 +4,22 @@ Opinionated template for [Rust](https://www.rust-lang.org) projects using [Twili
 
 ## ✨ Features
 
+### Common Twilight Crates Included
+
+Boilerplate for every Twilight crate with every feature enabled is in this template, you can simply remove the things you don't need
+
+For Twilight Gateway and Twilight HTTP, `rustls-webpki-roots`, `simd-json`, `zlib-simd` and `trust-dns` are enabled
+
+These crates aren't included because they aren't used very often:
+- `twilight-gateway-queue`
+- `twilight-http-ratelimiting`
+- `twilight-lavalink`
+
 ## 🤔 Usage
 
 1. Click on _Use this template_ in the GitHub web UI
    - > Or copy-paste the code in some other way
-2. Replace occurrences of `FILLME` with the actual value
+2. Replace occurrences of `FILLME` with the actual values
 
 ## 🙋 Wanted: Issues
 
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,48 @@
-fn main() {}
+use futures_util::StreamExt;
+use twilight_cache_inmemory::{InMemoryCache, ResourceType};
+use twilight_gateway::stream::ShardEventStream;
+use twilight_http::Client;
+use twilight_model::gateway::Intents;
+
+#[tokio::main]
+async fn main() {
+    let token = "FILLME".to_owned();
+
+    let client = Client::new(token.clone());
+
+    let mut shards = twilight_gateway::stream::create_recommended(
+        &client,
+        twilight_gateway::Config::new(token, Intents::empty()),
+        |_, builder| builder.build(),
+    )
+    .await
+    .unwrap()
+    .collect::<Vec<_>>();
+
+    let mut event_stream = ShardEventStream::new(shards.iter_mut());
+
+    let cache = InMemoryCache::builder()
+        .resource_types(ResourceType::empty())
+        .build();
+
+    while let Some((_, event_res)) = event_stream.next().await {
+        match event_res {
+            Ok(event) => {
+                cache.update(&event);
+                tokio::spawn(async move {
+                    println!("{event:#?}");
+                })
+            }
+            Err(err) => {
+                eprintln!("error receiving event: {err}");
+
+                if err.is_fatal() {
+                    eprintln!("received fatal error, exiting");
+                    break;
+                }
+
+                continue;
+            }
+        };
+    }
+}
